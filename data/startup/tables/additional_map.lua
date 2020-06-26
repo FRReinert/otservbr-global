a752ad891098aea6a93058e49a687d41d8dbb838
frreinert <fabricio.reinert@live.com>
2020-06-26 15:04:00 -0300

    added custom map

--- data/startup/tables/additional_map.lua
+++ data/startup/tables/additional_map.lua
@@ -1,3 +1,4 @@
 CustomMapTable = {
-	[1] = {file = "data/world/custom/otservbr-custom.otbm", enabled = true}
+	[1] = {file = "data/world/custom/otservbr-custom.otbm", enabled = true},
+	[2] = {file = "data/world/tupamar/tupamaru.otbm", enabled = true}
 }
